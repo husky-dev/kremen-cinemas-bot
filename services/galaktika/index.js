bfb4f2ef35f04eb0be5d5751b925e778faadedbe
Jaroslav Khorishchenko <websnipter@gmail.com>
2017-12-31 14:56:37 +0200

    service improved

---
+++ services/galaktika/index.js
@@ -0,0 +1,37 @@
+// Consts
+const {name, version, description} = require('./package.json');
+const {errors} = require('./common/consts.js');
+// Libs
+const {getSchedule} = require('./lib/parser.js');
+// Common
+const {asyncWrap} = require('./common/async.js');
+const log = require('./common/log.js').withModule('app');
+
+// App
+const app = require('express')();
+// Adding custom responses
+require('./common/responses.js')(app, name);
+
+// Info
+log.info('v' + version);
+log.info(description);
+
+// Root
+app.get('/', (req,res) => {
+  res.result({name, version, description});
+});
+
+app.get('/getSchedule', asyncWrap(async (req,res) => {
+  res.result(await getSchedule());
+}));
+
+// Default response
+app.all('/*', (req,res) => {
+  res.err({name: errors.UNKNOW_API_ENDPOINT, descr: {url: req.url, method: req.method}});
+});
+
+// Run
+const appPort = process.env.PORT || 8080;
+app.listen(appPort, () => {
+	log.info('listening on port: ' + appPort);
+});
